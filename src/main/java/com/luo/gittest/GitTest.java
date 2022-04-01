11ae45717a59a38b8df7f80a5c2e932e3a5b1bad
luo <2770997350@qq.com>
2022-04-01 20:46:57 +0800

    first commit

---
+++ src/main/java/com/luo/gittest/GitTest.java
@@ -0,0 +1,14 @@
+package com.luo.gittest;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author l'l
+ * @Date 2022/4/1 20:41
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
